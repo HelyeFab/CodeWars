78a53d109cf7737a3394770121c38afe2c863890
HelyeFab <emmanuelfabiani23@gmail.com>
2021-02-01 11:45:34 +0000

    challenge on pin number

---
+++ regex.js
@@ -0,0 +1,18 @@
+function checkNumsOnly(input)
+   {
+      let  numbers = /^[0-9]+$/;
+      if(input.match(numbers)&& input.length === 4)
+      {
+      console.log('Your Registration number has been accepted....');
+      return 
+      }
+      else
+      {
+      console.log('Pin should be numeric characters only and only 4 digits long');
+    //   return false;
+      }
+   } 
+
+
+   checkNumsOnly("2815")
+
