dfa8b67232bdd0d6dc1f8e1bf35c81166d89ba7f
HelyeFab <emmanuelfabiani23@gmail.com>
2021-02-02 17:26:35 +0000

    challenge on sumOfNumbers

---
+++ sumOfNumbers.js
@@ -0,0 +1,14 @@
+function getSum(a, b) {
+  let counter = 1
+  let sum = 0
+  for (i = 0; i <=b; i++) {
+    counter++
+    
+    let index = counter
+    sum = index + counter
+    
+  }
+  return sum
+}
+
+console.log( getSum(1, 3));
